69b620d19f22ae36f15afde9b6c6ae1cf03d5bf5
Rishikesh Kotgirwar <kotgirwarrishikesh@gmail.com>
2026-04-12 22:02:54 +0530

    fix main.jsx syntax

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -19,25 +19,4 @@ createRoot(document.getElementById('root')).render(
       <App />
     </Auth0Provider>
   </StrictMode>,
-)import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Auth0Provider } from '@auth0/auth0-react'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Auth0Provider
-      domain={import.meta.env.VITE_AUTH0_DOMAIN}
-      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-        audience: import.meta.env.VITE_AUTH0_AUDIENCE
-      }}
-      cacheLocation="localstorage"
-      useRefreshTokens={true}
-    >
-      <App />
-    </Auth0Provider>
-  </StrictMode>,
 )
